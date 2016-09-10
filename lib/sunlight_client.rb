2f41d3794d9c85c31bcb9b0af7d6d82b3d269ad0
Robert Baldwin <robert.baldwin@tapjoy.com>
2016-09-10 12:34:08 -0700

    sunlight client uses options hash

--- lib/sunlight_client.rb
+++ lib/sunlight_client.rb
@@ -5,36 +5,42 @@ class SunlightClient
   API_KEY = "ffc9a6f15c2940399e13d04651a8b999"
   BASE_URL = "http://congress.api.sunlightfoundation.com"
 
-  def self.get(keyword)
+  def self.get(keyword, options={})
     raise ArgumentError unless %i(legislators bills votes).include?(keyword)
 
-    first_page, last_page = get_page_range(keyword)
+    first_page = options.fetch(:page) { 1 }
+    last_page =  options.delete(:last_page) { page_count(keyword, options) }
 
-    collection = (first_page.to_i).upto(last_page.to_i).inject([]) do |collection, page|
-      print '.'
-      data = get_content("#{url_for(keyword)}&page=#{page}")
+    collection = (first_page).upto(last_page).inject([]) do |collection, page|
+      print page
+      data = get_content(url_for(keyword, options.merge(page: page)))
+      puts data
       collection += data['results']
     end
   end
 
   def self.get_first(keyword, options={})
     raise ArgumentError unless %i(legislators bills votes).include?(keyword)
-    url = URI(url_for(keyword))
-    url.query = URI.encode_www_form(options.merge(apikey: API_KEY))
+    url = url_for(keyword, options)
     get_content(url)["results"].first
   end
 
-  def self.get_page_range(keyword)
-    metadata = get_content(url_for(keyword))
-    [metadata['page']['page'], metadata['page']['count']]
+  def self.page_count(keyword, options)
+    metadata = get_content(url_for(keyword, options))
+    pages = metadata['count'] / metadata['page']['per_page']
+    pages.zero? ? 1 : pages
   end
 
   def self.get_content(url)
     JSON.parse(Net::HTTP.get(URI(url)))
   end
 
-  def self.url_for(keyword)
-    options = '&congress=114' if %i(bills votes).include?(keyword)
-    "#{BASE_URL}/#{keyword}?apikey=#{API_KEY}"
+  def self.url_for(keyword, options={})
+    options.merge!(apikey: API_KEY)
+
+    url = URI("#{BASE_URL}/#{keyword}")
+    url.query = URI.encode_www_form(options)
+    puts url
+    url
   end
 end
